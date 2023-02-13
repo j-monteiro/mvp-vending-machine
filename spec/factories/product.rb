5de094b1dabb814b4aebd5825fbc16c6236a0a05
João Monteiro <jlmonteiro95@gmail.com>
2023-02-13 01:11:17 +0000

    fixup! fix: user - product relation

--- spec/factories/product.rb
+++ spec/factories/product.rb
@@ -6,5 +6,6 @@ FactoryBot.define do
     cost { Faker::Number.decimal(l_digits: 2) }
     product_name { Faker::Food.dish }
     reference { Faker::Internet.uuid }
+    user { build(:user, role: 'seller') }
   end
 end
